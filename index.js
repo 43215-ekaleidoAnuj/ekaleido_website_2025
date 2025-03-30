2ec77afe81756857605179e99b59c875cc0869eb
bt2004nk <bt2004nk@gmail.com>
2025-03-30 20:31:52 +0530

    ui changes

--- index.js
+++ index.js
@@ -8,6 +8,30 @@ $(document).ready(function(){
    infinite: true, // Infinite looping
      arrows: true // Show navigation arrows
    });
+   $('.carouselDrivingtwo').slick({
+    dots: false,
+    arrows: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true, // Ensures middle slide can be styled uniquely
+    centerPadding: "0", // Remove extra spacing
+ });
+ $(".carouselDrivingtwo").on("afterChange", function (event, slick, currentSlide) {
+  let centerSlide = Math.floor(slick.$slides.length / 2); // Get the center slide index
+  let selectedImageSrc = $(".carouselDrivingtwo .slick-center img").attr("src");
+
+  if (selectedImageSrc) {
+    $("#selected-image").attr("src", selectedImageSrc);
+  }
+});
+
+// Initial update on page load
+let initialImageSrc = $(".carouselDrivingtwo .slick-center img").attr("src");
+if (initialImageSrc) {
+  $("#selected-image").attr("src", initialImageSrc);
+}
  });
  
+ 
  // Slick version 1.5.8
+ 
